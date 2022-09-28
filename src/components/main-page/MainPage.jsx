a579d504da001bd30e3c6c3b64928992bcafca04
Cameron <cameronhunt88@hotmail.com>
2022-09-28 17:03:26 +0100

    installed sass, started building mainpage

---
+++ src/components/main-page/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./main-page.scss";
+
+function MainPage() {
+	return (
+		<React.Fragment>
+			<section id="wrapper">
+				<h1>Test</h1>
+			</section>
+		</React.Fragment>
+	);
+}
+
+export default MainPage;
