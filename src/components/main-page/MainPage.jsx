2eaf2ff8bb784b9e44089fc111482b9bd2e83fcd
CamyH <cameronhunt88@hotmail.com>
2022-09-30 12:46:45 +0100

    added more styling + assets

--- src/components/main-page/MainPage.jsx
+++ src/components/main-page/MainPage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./main-page.scss";
+import accountIcon from ""
 
 function MainPage() {
 	return (
 		<React.Fragment>
+			<img class="" />
 			<section id="wrapper">
 				<h1 id="header">StockTrack</h1>
 				<input id="search-bar" type="text" placeholder="Enter a stock here" />
+				<button class="button">Track Stock</button>
 			</section>
 		</React.Fragment>
 	);
