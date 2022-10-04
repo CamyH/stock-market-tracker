bd79b77c6d3173ae13525bed0c17fe0bd291f6ab
CamyH <cameronhunt88@hotmail.com>
2022-10-04 19:44:02 +0100

    changed api slightly

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 // API - Alpha Vantage // https://www.alphavantage.co/documentation/
 // https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo
+// Below code modified from documentation
 
 let request = require('request');
 let key = 'RSZVV5D6GXX5BHZO';
-let api = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=' + key;
+let api = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=' + key;
 
 
 request.get({
