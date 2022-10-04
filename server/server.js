cbc43ce5df882877f16d2382f03ab86150f71c3d
CamyH <cameronhunt88@hotmail.com>
2022-10-04 19:17:57 +0100

    boilerplate api request + request module

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-// API handling to go here
+// API - Alpha Vantage // https://www.alphavantage.co/documentation/
+// https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo
+
+let request = require('request');
+let api = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AAPL&apikey=RSZVV5D6GXX5BHZO';
+
+
+request.get({
+    url: api,
+    json: true,
+    headers: {"User-Agent": "request"}
+}, (err, res, data) => {
+    if (err) {
+        console.log("Error: ", err);
+    } else if (res.statusCode !== 200) {
+        console.log("Status: ", res.statusCode);
+    } else {
+        // data successfully parsed
+        console.log(data);
+    }
+});
