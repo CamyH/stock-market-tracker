e75e566fb0d1774c7f991353bd93fe0b930308e4
Cameron <cameronhunt88@hotmail.com>
2022-10-10 11:21:16 +0100

    added comment

--- client/src/components/chart-js/LineChart.jsx
+++ client/src/components/chart-js/LineChart.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+// Without this import statement chart will not work
 import { Chart as ChartJS } from "chart.js/auto";
 
 function LineChart({ chartData }) {
